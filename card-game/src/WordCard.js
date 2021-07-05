890d62afaaf10b9e6367dc64788e288c5781443d
6210110349 <6210110349@psu.ac.th>
2021-07-05 11:52:21 +0700

    3SA03 : part6 wordCard update

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import CharacterCard from './CharacterCard';
-export default function WordCard(props){
- const activationHandler = c => { console.log(`${c} has been activated.`) }
- return (
- <div>
-{ Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}/>) }
- </div>
- );
+import _ from 'lodash';
+import React,{useState} from 'react';
+ 
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
+    }
+}
+const activationHandler = (c) => {
+    console.log(`${c} has been activated.`)
+    let guess = state.guess + c
+    setState({...state, guess})
+    if(guess.length == state.word.length){
+        if(guess == state.word){
+            console.log('yeah!')
+            setState({...state, guess: '', completed: true})
+        }
+        else{
+            console.log('reset')
+            setState({...state, guess: '', attempt: state.attempt + 1})
+     }
+    }
 }
