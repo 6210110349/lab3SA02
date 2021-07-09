38c5943d8c87dc2750e09c9634e48f8785a7e32c
6210110349 <6210110349@psu.ac.th>
2021-07-09 23:33:49 +0700

    ิอัพเดตคำอธิบาย

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -7,8 +7,9 @@ function App() {
  return (
  <div className ={'body'}>
    <div>
-      <header >
-       <h1 className ={'header'} >RAMDOMCARD</h1>
+      <header className ={'header'} >
+       <h1  >RAMDOMCARD</h1>
+       <p>Caption : สุ่มการ์ดซึ่งประกอบไปด้วยการ์ด x ที่จะลด blood และ s ที่จะเพิ่ม score และเมื่อค่า blood เหลือ 0 จะถือว่าแพ้ </p>
       </header>
    </div>
    <div><WordCard value={word}/></div>
